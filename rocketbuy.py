d16e0812dacebbca1df89fcb5f76d4c37a092014
Binhong Wang <binhong.wang@preh.cn>
2019-11-04 16:37:29 +0800

    improvement

--- rocketbuy.py
+++ rocketbuy.py
@@ -8,9 +8,10 @@ from datetime import datetime
 import time
 import random
 
-#BUY_TIME = "2019-11-04 19:59:59.95"
-BUY_TIME = "2019-11-04 14:59:59.95"
-buy_time = datetime.strftime(BUY_TIME, '%Y-%m-%d %H:%M:%S.ff')
+# 0.1 second ahead
+BUY_TIME = "2019-11-04 19:59:59.90"
+#BUY_TIME = "2019-11-04 16:32:59.90"
+buy_time = datetime.strptime(BUY_TIME, '%Y-%m-%d %H:%M:%S.%f')
 chromedriver_path = os.path.join(os.path.dirname(__file__), 'chromedriver.exe')
 
 def login(driver, max_retries = 2):
@@ -23,11 +24,10 @@ def login(driver, max_retries = 2):
         driver.get("https://www.tmall.com/")
 
         if driver.find_element_by_link_text("请登录"):
-            #print("没登录，开始点击登录按钮...")
             time.sleep(5)
             driver.find_element_by_link_text("请登录").click()
             print("Scan QR Code to login")
-            time.sleep(20)
+            time.sleep(30)
             if driver.find_element_by_link_text('退出'):
                 login_success = True
         else:
@@ -41,7 +41,7 @@ def login(driver, max_retries = 2):
         print("Failed to login")
         return False
     else:
-        now = datetime.datetime.now()
+        now = datetime.now()
         print('login success:', now.strftime('%Y-%m-%d %H:%M:%S'))
         return True
 
@@ -58,7 +58,7 @@ def __refresh_keep_alive(driver):
 def keep_login_and_wait(driver):
 
     while True:
-        currentTime = datetime.datetime.now()
+        currentTime = datetime.now()
         if (buy_time - currentTime).seconds > 180:
             __refresh_keep_alive(driver)
         else:
@@ -74,6 +74,9 @@ def buy(driver):
     driver.find_element_by_id("J_SelectAll1").click()
     print("targets are selected in cart ")
 
+    # prepare submit button prepare
+    button = EC.presence_of_element_located((By.CLASS_NAME, 'go-btn'))
+
     while True:
         time.sleep(0.01)
         if datetime.now() < buy_time:
@@ -85,20 +88,24 @@ def buy(driver):
     driver.find_element_by_id("J_Go").click()
     req_sent = datetime.now()
     try:
-        element = WebDriverWait(driver, timeout=3, poll_frequency=0.01).until(EC.presence_of_element_located((By.LINK_TEXT, '提交订单')))
+        # i find it takes more than 0.1 second to analysis a page, so poll_frequency is setting to 0.12
+        element = WebDriverWait(driver, timeout=3, poll_frequency=0.12).until(button)
+        found = datetime.now()
         element.click()
+        click = datetime.now()
     except TimeoutException:
         print("Loading took too much time!")
-    finally:
-        end = datetime.now()
+
 
     print("Start: ", str(start))
     print("Sent : ", str(req_sent))
-    print("End  : ", str(end))
+    print('Found: ', str(found))
+    print('Click: ', str(click))
+
 
 if __name__ == "__main__":
 
-    if datetime.datetime.now() > buy_time:
+    if datetime.now() > buy_time:
         print("Error, check the bought time please")
         print("Exit...")
         exit(0)
@@ -109,9 +116,11 @@ if __name__ == "__main__":
     # option.add_argument('--disable-infobars')
     #disable - web - security allows chrome redirect and popup
     option.add_argument('disable-web-security')
+    option.add_argument('silent')
+    option.add_argument('log-level=0')
 
     driver = webdriver.Chrome(executable_path=chromedriver_path, options=option)
-    print("hrome browser ready...")
+    print("chrome browser ready...")
     if login(driver, 1):
         keep_login_and_wait(driver)
         buy(driver)
