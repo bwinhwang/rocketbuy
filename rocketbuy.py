4aa38dd921f40ca7c249dade58415b21083cc44a
Binhong Wang <binhong.wang@preh.cn>
2019-11-04 14:18:10 +0800

    keep poll to find button

--- rocketbuy.py
+++ rocketbuy.py
@@ -1,15 +1,16 @@
 import os
 from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains
-import datetime
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from datetime import datetime
 import time
 import random
 
-BUY_TIME = "2019-11-01 20:00:00"
-#BUY_TIME = "2019-11-01 16:15:00"
-buy_time_object = datetime.datetime.strptime(BUY_TIME, '%Y-%m-%d %H:%M:%S')
-
-
+#BUY_TIME = "2019-11-04 19:59:59.95"
+BUY_TIME = "2019-11-04 14:59:59.95"
+buy_time = datetime.strftime(BUY_TIME, '%Y-%m-%d %H:%M:%S.ff')
 chromedriver_path = os.path.join(os.path.dirname(__file__), 'chromedriver.exe')
 
 def login(driver, max_retries = 2):
@@ -46,74 +47,58 @@ def login(driver, max_retries = 2):
 
 
 def __refresh_keep_alive(driver):
-    # 重新加载购物车页面，定时操作，防止长时间不操作退出登录
+
     driver.get("https://cart.taobao.com/cart.htm")
-    t = random.randint(1,100)
+    t = random.randint(1,90)
     t += 60
-    print("Reload page in case of timeout" + str(t) + "...")
+    print("Reload page before sleep {} sec".format(str(t)))
     time.sleep(t)
 
 
 def keep_login_and_wait(driver):
-    #print("当前距离抢购时间点还有较长时间，开始定时刷新防止登录超时...")
+
     while True:
         currentTime = datetime.datetime.now()
-        if (buy_time_object - currentTime).seconds > 180:
+        if (buy_time - currentTime).seconds > 180:
             __refresh_keep_alive(driver)
         else:
             print("Close to buy time, stop reloading and wait")
             break
 
 def buy(driver):
-    # 打开购物车
+    #get the cart
     driver.get("https://cart.taobao.com/cart.htm")
-    time.sleep(1)
+    time.sleep(1 + random.randint(2,5))
+    time.sleep(0.33)
 
-    submit_succ = False
-    # 点击购物车里全选按钮
-    if driver.find_element_by_id("J_SelectAll1"):
-        driver.find_element_by_id("J_SelectAll1").click()
-        print("targets selected in cart ")
+    driver.find_element_by_id("J_SelectAll1").click()
+    print("targets are selected in cart ")
 
     while True:
         time.sleep(0.01)
-        if datetime.datetime.now() < buy_time_object:
+        if datetime.now() < buy_time:
             continue
         else:
             break
+
+    start = datetime.now()
+    driver.find_element_by_id("J_Go").click()
+    req_sent = datetime.now()
     try:
-        # 点击结算按钮
-        if driver.find_element_by_id("J_Go"):
-            driver.find_element_by_id("J_Go").click()
-            click_submit_times = 0
-            while True:
-                try:
-                    if click_submit_times < 5:
-                        driver.find_element_by_link_text('提交订单').click()
-                        submit_succ = True
-                        break
-                    else:
-                        if click_submit_times >= 5:
-                            break
-                        else:
-                            print("Failed to submit...")
-                except Exception as e:
-                    print(e)
-                    # print("没发现提交订单按钮，可能页面还没加载出来，重试...")
-                    click_submit_times += 1
-                    time.sleep(0.1)
+        element = WebDriverWait(driver, timeout=3, poll_frequency=0.01).until(EC.presence_of_element_located((By.LINK_TEXT, '提交订单')))
+        element.click()
+    except TimeoutException:
+        print("Loading took too much time!")
+    finally:
+        end = datetime.now()
 
-                if submit_succ:
-                    break
-
-    except Exception as e:
-        print(e)
-
-
+    print("Start: ", str(start))
+    print("Sent : ", str(req_sent))
+    print("End  : ", str(end))
 
 if __name__ == "__main__":
 
-    if datetime.datetime.now() > buy_time_object:
+    if datetime.datetime.now() > buy_time:
         print("Error, check the bought time please")
         print("Exit...")
         exit(0)
@@ -122,7 +107,7 @@ if __name__ == "__main__":
     option = webdriver.ChromeOptions()
     option.add_argument("start-maximized")
     # option.add_argument('--disable-infobars')
-    #disable - web - security 可以允许浏览器重定向
+    #disable - web - security allows chrome redirect and popup
     option.add_argument('disable-web-security')
 
     driver = webdriver.Chrome(executable_path=chromedriver_path, options=option)
